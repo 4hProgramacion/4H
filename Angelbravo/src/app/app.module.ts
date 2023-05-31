4c594cbe0930c25a158960cd2c974a6eca52e1f6
Angeldejesusgonzalezbravo <angeldejesusgonzalezbravo98@gmail.com>
2023-05-31 09:39:48 -0500

    servicio

--- Angelbravo/src/app/app.module.ts
+++ Angelbravo/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { Eje1Component } from './ejercicios/eje1/eje1.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Eje2Component } from './ejercicios/eje2/eje2.component';
 import { Eje3Component } from './ejercicios/eje3/eje3.component';
+import { HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,8 @@ import { Eje3Component } from './ejercicios/eje3/eje3.component';
     AppRoutingModule,
     FormsModule,
     ReactiveFormsModule,
-    NgModule
+    NgModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
