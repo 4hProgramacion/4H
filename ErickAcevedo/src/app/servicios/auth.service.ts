8a1ee7b5c594660899db78c0218bebec8de295dd
ErickAcevedoRamon <almn.erickacevedo@cbtis66.edu.mx>
2023-05-30 21:48:51 -0600

    Update auth.service.ts

--- ErickAcevedo/src/app/servicios/auth.service.ts
+++ ErickAcevedo/src/app/servicios/auth.service.ts
@@ -5,12 +5,10 @@ import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
-export class AuthService {
-  protected url = 'http://localhost:8000/api4h/';
-
-  constructor(private rutas: HttpClient) {}
-
-  public login(usuario: string, password: string): Observable <any> {
-    this.rutas.get(this.url + '/login', {usuario, password});
-}
+  export class AuthService {
+    protected url= 'http://localhost:8000/api4h/';
+    constructor(private rutas: HttpClient) { }
+    public login(usuario: string, password: string): Observable<any>{
+      this.rutas.post(this.url + 'login', {usuario, password});
+    } 
 }
