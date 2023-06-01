a452e788d9db6faf1c135e6eeee9361d0173d82d
ErickAcevedoRamon <almn.erickacevedo@cbtis66.edu.mx>
2023-06-01 00:10:05 -0600

    servicio y base de datos

--- ErickAcevedo/src/app/servicios/auth.service.ts
+++ ErickAcevedo/src/app/servicios/auth.service.ts
@@ -5,10 +5,13 @@ import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
-  export class AuthService {
-    protected url= 'http://localhost:8000/api4h/';
-    constructor(private rutas: HttpClient) { }
-    public login(usuario: string, password: string): Observable<any>{
-      this.rutas.post(this.url + 'login', {usuario, password});
+  export class UsuariosServices {
+    protected url= 'http://localhost:8000/api/listadoUsuarios/';
+    constructor(private http: HttpClient) { 
+
+    } 
+    
+    public getUsuarios(): Observable<any>{
+      return this.http.get(this.url)
     } 
 }
