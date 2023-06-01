edcbdf05f1b72a5c47115345be97eae9a9d51571
MonicaMurciaLiborio <129346177+MonicaMurciaLiborio@users.noreply.github.com>
2023-05-31 20:40:14 -0600

    Creacion de un login

--- MonicaMurciaLiborio/src/app/ejercicios/eje1/eje1.component.ts
+++ MonicaMurciaLiborio/src/app/ejercicios/eje1/eje1.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-eje1',
@@ -6,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./eje1.component.css']
 })
 export class Eje1Component {
-
+  public title: string = 'Empresa de Monica SA de CV';
+  public correo: string = '';
+  public password: string = '';
+  
+  protected usuarios = [
+    { usr: "monica@l.com", psw: "1234", nombre: "Monica Murcia Liborio" },
+    { usr: "rubi@l.com", psw: "12345", nombre: "Rubi Ruiz Ramiez" },
+    { usr: "william@l.com", psw: "123456", nombre: "William de Jesús Noriega Damián" }
+  ];
+  
+  constructor(private rutas: Router) { }
+  
+  public acceso(): void {
+    for (let i = 0; i < this.usuarios.length; i++) {
+      if (this.correo == this.usuarios[i].usr && this.password == this.usuarios[i].psw) {
+        alert("Bienvenido al sistema " + this.title);
+        this.rutas.navigate(["/Home"]);
+        return;
+      }
+      alert("El usuario o la contraseña no son correctas");
+    }
+  }
 }
