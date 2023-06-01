4750569643e10f430867c44fe3ccca3d6e22c074
AxelManuelXolioMorales <129126736+AxelManuelXolioMorales@users.noreply.github.com>
2023-06-01 18:12:19 -0500

    Revert "base de datos conectada"
    This reverts commit ccbce9c96ec034d9a5ad8dbff6c3e60c35c838ad.

--- AxelXolio/src/app/ejercicios/eje2/eje2.component.ts
+++ AxelXolio/src/app/ejercicios/eje2/eje2.component.ts
@@ -1,26 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { UsuariosService } from 'src/app/servicios/auth.service';
-
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-eje2',
   templateUrl: './eje2.component.html',
   styleUrls: ['./eje2.component.css']
 })
-export class Eje2Component implements OnInit {
-
-  constructor(private UsuarioServices: UsuariosService) {}
-  public usuarios: any = [];
-  ngOnInit(): void {
-    this.UsuarioServices.getUsuarios().subscribe (
-      (res: any) => {
-        this.usuarios = res;
-      },
-      (err) => {
-        console.log(err);
-      }
-    );
-  }
-  
+export class Eje2Component {
 
 }
