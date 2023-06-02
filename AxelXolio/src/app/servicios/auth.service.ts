74dc9956d5af6bf5f30f1b712c602b4f2d248904
AxelManuelXolioMorales <129126736+AxelManuelXolioMorales@users.noreply.github.com>
2023-06-01 23:51:50 -0500

    Revert "base de datos (error?)"
    This reverts commit 50ab150b0df018287892027a4f1dd906a125eddd.

--- AxelXolio/src/app/servicios/auth.service.ts
+++ AxelXolio/src/app/servicios/auth.service.ts
@@ -5,10 +5,11 @@ import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
-export class AuthService {
-  protected url = 'http://localhost:8000/api4h';
-  constructor(private http: HttpClient){}
-  public login(usuario: string, password: string): Observable<any>{
-    return this.http.post(this.url + 'login', { usuario, password });
+export class UsuariosService {
+  protected url = 'http://localhost:8000/api/ListadoUsuarios/';
+  constructor(private http: HttpClient) {}
+
+  public getUsuarios():Observable<any>{
+    return this.http.get(this.url);
   }
 }
