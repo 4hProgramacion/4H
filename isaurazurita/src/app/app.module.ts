6a546d59cc143e68d43535b69abca5b78679fb89
isa ★ <almn.isaurazurita@cbtis66.edu.mx>
2023-06-01 20:30:27 -0600

    Creación del login

--- isaurazurita/src/app/app.module.ts
+++ isaurazurita/src/app/app.module.ts
@@ -3,18 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { Ejer1Component } from './ejercicios/ejer1/ejer1.component';
 import { Eje1Component } from './ejercicios/eje1/eje1.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Ejer1Component,
-    Eje1Component
+Eje1Component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    formsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
