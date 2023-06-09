160f397e2ffb7720430f39c802e274929bf1e856
isa ★ <almn.isaurazurita@cbtis66.edu.mx>
2023-06-08 20:54:26 -0600

    Creación del eje2

--- isaurazurita/src/app/ejercicios/eje1/eje1.component.ts
+++ isaurazurita/src/app/ejercicios/eje1/eje1.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Route, Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-eje1',
@@ -14,10 +15,12 @@ export class Eje1Component {
                        {usr:"rubi@l.com",psw:"12345", nombre:"Rubi Ruíz Ramirez"},
                        {usr:"murcia@l.com",psw:"123456", nombre:"Monica Murcia Ligorio"}];
 
+constructor(private rutas:Router){ }
   validar(){
   for(let i=0; i<this.usuarios.length; i++){
 if(this.email==this.usuarios[i].usr && this.password==this.usuarios[i].psw){
     alert(this.usuarios[i].nombre+" Bienvenido al sistema :) " + this.title);
+    this.rutas.navigate(["/Home"]);
     return;
   }
     alert("El usuario y contraseña no son correctos :(");
