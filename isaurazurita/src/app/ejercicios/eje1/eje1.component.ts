d68258cb77e81bbfdb13d04e31afee213dbf6545
isa ★ <almn.isaurazurita@cbtis66.edu.mx>
2023-06-08 20:22:02 -0600

    Validación de usuario

--- isaurazurita/src/app/ejercicios/eje1/eje1.component.ts
+++ isaurazurita/src/app/ejercicios/eje1/eje1.component.ts
@@ -10,12 +10,16 @@ export class Eje1Component {
   protected title: string = 'Empresa de isaura SA de CV';
   protected email: string = '';
   protected password: string = '';
+  protected usuarios= [ {usr:"isaura@l.com",psw: "1234", nombre:"Isaura Zurita Guerrero"},
+                       {usr:"rubi@l.com",psw:"12345", nombre:"Rubi Ruíz Ramirez"},
+                       {usr:"murcia@l.com",psw:"123456", nombre:"Monica Murcia Ligorio"}];
 
   validar(){
-    if(this.email=="isaura@l.com" && this.password=="1234"){
-    alert("Bienvenido al sistema :) " + this.title);
+  for(let i=0; i<this.usuarios.length; i++){
+if(this.email==this.usuarios[i].usr && this.password==this.usuarios[i].psw){
+    alert(this.usuarios[i].nombre+" Bienvenido al sistema :) " + this.title);
+    return;
   }
-  else{
     alert("El usuario y contraseña no son correctos :(");
   }
 }
