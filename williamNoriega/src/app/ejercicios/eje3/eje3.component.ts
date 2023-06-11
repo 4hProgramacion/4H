019ce9341ae7aa4406d69f6e996511ef80e83ac3
WilliamNoriega <129239929+WilliamNoriega@users.noreply.github.com>
2023-06-11 16:45:34 -0600

    enviar datos a la base de datos

--- williamNoriega/src/app/ejercicios/eje3/eje3.component.ts
+++ williamNoriega/src/app/ejercicios/eje3/eje3.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UsuariosService } from 'src/app/servicios/usuarios.service';
 
 @Component({
   selector: 'app-eje3',
@@ -8,32 +9,92 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class Eje3Component {
   protected title: string = "Empresa de William SA de CV";
-  passwordForm: FormGroup;
-  constructor(private formbuilder: FormBuilder){
-    this.passwordForm = this.formbuilder.group({
-      password: ['',[
+  formulario: FormGroup;
+  
+  constructor(private formbuilder: FormBuilder, private servicio: UsuariosService){
+    this.formulario = this.formbuilder.group({
+      password: ['', [
         Validators.required,
-        Validators.minLength(8),
         Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]+$'),
-      ]],
-      confirmaPassword: ['', [
-        Validators.required,
-      ]],
-      correo: ['', [
-        Validators.required,
-        Validators.email,
-      ]],
-      nombre: ['', [
-        Validators.required,
-      ]]
-    },
-    { Validators: this.passwordIguales });
+        Validators.minLength(8)
+    ]],
+    confirmaPassword:['',[
+      Validators.required
+    ]],
+    email:['',[
+      Validators.required,
+      Validators.email
+    ]],
+    nombre:['', [
+      Validators.required,
+      Validators.pattern('^[a-zA-Z]*$')
+    ]],
+    edad:['', [
+      Validators.required,
+      Validators.pattern('^[0-9]*$'),
+      Validators.min(18),
+      Validators.max(120)
+    ]],
+    telefono:['', [
+      Validators.required,
+      Validators.pattern('^[0-9]*$'),
+      Validators.minLength(10),
+      Validators.maxLength(10)
+    ]],
+    direccion:['', [
+      Validators.required
+    ]],
+    ciudad:['', [
+      Validators.required
+    ]],
+    estado:['', [
+      Validators.required
+    ]],
+    pais:['', [
+      Validators.required
+    ]],
+    codigoPostal:['', [
+      Validators.required,
+      Validators.pattern('^[0-9]*$')
+    ]],
+    fecha:['', [
+      Validators.required
+    ]],
+
+  },
+      { validators: this.passwordIguales('password', 'confirmaPassword') }
+    );
   }
 
-  protected passwordIguales(formGroup: FormGroup){
-    const pass = formGroup.get('password')?.value || '';
-    const confirmaPassword = formGroup.get('confirmaPassword')?.value || '';
+  protected passwordIguales(pass1: string, pass2: string){
+    return (formGroup: FormGroup) => {
+      const pass1Control = formGroup.controls[pass1];
+      
+      const pass2Control = formGroup.controls[pass2];
 
-    return pass === confirmaPassword ? null : { noSonIguales: true };
+      if(pass1Control.value === pass2Control.value){
+        
+        formGroup.controls['confirmaPassword'].setErrors(null);
+      
+      } else {
+        
+        formGroup.controls['confirmaPassword'].setErrors({ noSonIguales: true });
+      
+      }
+    }
+  }
+
+  public enviarDatos(){
+    console.log(this.formulario.value);
+    alert("Datos enviados Correctamente");
+
+    this.servicio.RegistrarUsuario(this.formulario.value).subscribe(
+      (response) => {
+        console.log(response);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
   }
 }
