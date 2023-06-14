1dd78db53f834a8c8d524c62a1fc0bc0a81c2385
dochtml <88597899+dochtml@users.noreply.github.com>
2023-06-13 22:31:41 -0600

    Eje 1 - login / 3er Parcial Terminado
    Cambios:
    - Eje 1 Configurado para iniciar sesión
    - Guardado en LocalStorange

--- guillermoHeribertoLopezPerez/src/app/servicios/auth.service.ts
+++ guillermoHeribertoLopezPerez/src/app/servicios/auth.service.ts
@@ -7,11 +7,11 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class AuthService {
-  /*protected url = 'http://localhost:8000/api4h';
+  protected url = 'http://localhost:8000/api';
   
   constructor(private http: HttpClient){}
   
-  public login(usuario: string, password: string): Observable<any>{
-    this.http.post(this.url + 'login', { usuario, password});
-  }*/
+  public login(user: any): Observable<any>{
+    return this.http.post(this.url + 'login', user );
+  }
 }
