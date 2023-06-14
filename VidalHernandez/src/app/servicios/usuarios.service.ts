b64f13410a8b955d5c906345187c31a550bf4551
LuisVidalHernandez <134672495+LuisVidalHernandez@users.noreply.github.com>
2023-06-13 21:15:11 -0600

    Datos de registro a la base de datos

--- VidalHernandez/src/app/servicios/usuarios.service.ts
+++ VidalHernandez/src/app/servicios/usuarios.service.ts
@@ -1,14 +1,20 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import {HttpClient} from '@angular/common/http';
+import {Injectable} from '@angular/core';
+import {Observable} from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
+
 export class UsuariosService {
   protected url = 'http://localhost:8000/api/ListadoUsuarios/';
-  constructor(private usuario: HttpClient) {}
-  public getUsuarios():Observable<any>{
-    return this.usuario.get(this.url);
+  constructor(private http: HttpClient) {}
+
+  public getUsuarios(): Observable <any>{
+    return this.http.get(this.url + 'ListadoUsuarios');
+  }
+  
+  public RegistrarUsuario(usuario: any): Observable<any>{
+    return this.http.post(this.url + 'RegistroUsuario',usuario);
   }
 }
