a6e509ccfe5d5ab730d7a4ec2c57a36248727d9b
AxelManuelXolioMorales <129126736+AxelManuelXolioMorales@users.noreply.github.com>
2023-06-14 23:50:52 -0500

    Eje 1 - login / 3er Parcial Terminado

--- AxelXolio/src/app/app.module.ts
+++ AxelXolio/src/app/app.module.ts
@@ -4,8 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Eje1Component } from './ejercicios/eje1/eje1.component';
-import { Eje2Component } from './ejercicios/eje2/eje2.component';
 import { Eje3Component } from './ejercicios/eje3/eje3.component';
+import { Eje2Component } from './ejercicios/eje2/eje2.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
 
