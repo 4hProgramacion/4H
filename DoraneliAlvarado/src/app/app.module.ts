5d8a0becb85e3093d6e0592238e9964f7a50d0ee
Doranelialvarado <almn.doranelialvarado@cbtis66.edu.mx>
2023-06-16 17:10:42 -0600

    Modificación de eje1 y correcciones

--- DoraneliAlvarado/src/app/app.module.ts
+++ DoraneliAlvarado/src/app/app.module.ts
@@ -3,15 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { Eje1Component } from './ejercicios/eje1/eje1.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Eje2Component } from './ejercicios/eje2/eje2.component';
 import {HttpClientModule} from '@angular/common/http';
 import { Ejercicio3Component } from './ejercicios/eje3/eje3.component';
+import { Ejercicio1Component } from './ejercicios/eje1/eje1.component';
 @NgModule({
   declarations: [
     AppComponent,
-    Eje1Component,
+    Ejercicio1Component,
     Eje2Component,
     Ejercicio3Component
   ],
