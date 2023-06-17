e3af21830f60bfd77a0ab0ab25db41b33cacf01e
Gontran Huesca Marquez <127902677+GontranHuesca@users.noreply.github.com>
2023-06-16 23:10:40 -0600

    hola a todos eje 2

--- GontranHuesca/src/app/app-routing.module.ts
+++ GontranHuesca/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Ejer1Component } from './ejercicios/ejer1/ejer1.component';
+import { Eje2Component } from './ejercicios/eje2/eje2.component';
 
 const routes: Routes = [
   {path: 'eje1', component:Ejer1Component},
