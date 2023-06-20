6a9de335b02ca1ce9ad2be30fc2cda05a9013dbf
DiegoDeJesusGutierrezGranillo <136788364+DiegoDeJesusGutierrezGranillo@users.noreply.github.com>
2023-06-20 11:25:11 -0600

    Commit

--- DiegoGutierrez/src/app/app.module.ts
+++ DiegoGutierrez/src/app/app.module.ts
@@ -3,14 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Eje1Component } from './ejercicios/eje1/eje1.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { Eje2Component } from './ejercicios/eje2/eje2.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Eje1Component,
+    Eje2Component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
